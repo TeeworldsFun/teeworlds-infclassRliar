e34bf256de30e1c2e0dd983c0ed282aaff4e011e
ST-Chara <chara_kill_sans@163.com>
2021-11-07 19:50:37 +0800

    :(

---
+++ config.lua
@@ -0,0 +1,11 @@
+compiler.driver = 'gcc'
+compiler.c_compiler = false
+compiler.cxx_compiler = false
+stackprotector.value = true
+minmacosxsdk.value = false
+macosxppc.value = false
+zlib.value = true
+zlib.include_path = false
+mysql.value = true
+mysql.use_mysqlconfig = true
+nogeolocation.value = false
---
+++ src/game/generated/nethash.cpp
@@ -0,0 +1,1 @@
+#define GAME_NETVERSION_HASH "bfa4d5b55f42e5a3"
