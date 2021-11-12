26722b080289a6803da4332aa4545814f64abd80
ST-Chara <chara_kill_sans@163.com>
2021-11-12 22:38:27 +0800

    Try again

--- infc.sql
+++ infc.sql
@@ -3,23 +3,23 @@
 DROP TABLE IF EXISTS `tw_Users`;
 CREATE TABLE IF NOT EXISTS `tw_Users` (
 	`UserId` INT NOT NULL AUTO_INCREMENT, 
-	`Username` VARCHAR(64) BINARY NOT NULL, 
-	`Email` VARCHAR(64) BINARY NOT NULL, 
-	`PasswordHash` VARCHAR(64) BINARY NOT NULL, 
+	`Username` VARCHAR(64) NOT NULL, 
+	`Email` VARCHAR(64) NOT NULL, 
+	`PasswordHash` VARCHAR(64) NOT NULL, 
 	`Level` INT DEFAULT '0' NOT NULL, 
-	`RegisterDate` DATETIME NOT NULL, 
+	`RegisterDate` DATETIME NOT NULL DEFAULT NOW(), 
 	`RegisterIp` VARCHAR(64) NOT NULL, 
     PRIMARY KEY (`UserId`)
-) CHARACTER SET utf8 ;
+) CHARSET=utf8;
 
 DROP TABLE IF EXISTS `tw_infc_Rounds`;
 CREATE TABLE IF NOT EXISTS `tw_infc_Rounds` (
     `RoundId` INT NOT NULL AUTO_INCREMENT, 
-    `MapName` VARCHAR(64) BINARY NOT NULL, 
+    `MapName` VARCHAR(64) NOT NULL, 
     `NumPlayersMin` INT NOT NULL, 
     `NumPlayersMax` INT NOT NULL, 
     `NumWinners` INT NOT NULL, 
-    `RoundDate` DATETIME NOT NULL, 
+    `RoundDate` DATETIME NOT NULL DEFAULT NOW(), 
     `RoundDuration` INT NOT NULL, 
     PRIMARY KEY (`RoundId`)
 ) CHARSET=utf8;
@@ -29,9 +29,9 @@ CREATE TABLE IF NOT EXISTS `tw_infc_RoundScore` (
     `RoundScoreId` INT NOT NULL AUTO_INCREMENT, 
     `UserId` INT NOT NULL, 
     `RoundId` INT NOT NULL, 
-    `MapName` VARCHAR(64) BINARY NOT NULL, 
+    `MapName` VARCHAR(64) NOT NULL, 
     `ScoreType` INT NOT NULL, 
-    `ScoreDate` DATETIME NOT NULL, 
+    `ScoreDate` DATETIME NOT NULL DEFAULT NOW(), 
     `Score` INT NOT NULL, 
     PRIMARY KEY (`RoundScoreId`), 
     FOREIGN KEY (`UserId`) REFERENCES tw_Users(`UserId`), 
