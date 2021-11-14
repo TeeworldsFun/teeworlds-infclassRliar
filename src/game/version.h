258f3cf8190f91e2125dee02e0da45cc40248bf5
ST-Chara <chara_kill_sans@163.com>
2021-11-14 21:32:30 +0800

    nethash is toooo bad

--- src/game/version.h
+++ src/game/version.h
@@ -3,9 +3,9 @@
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
 #include "generated/nethash.cpp"
-#define GAME_NETVERSION_HASH_FORCED "868ccbe551010094"
 #define GAME_VERSION "0.6.4"
-#define GAME_NETVERSION "0.6 " GAME_NETVERSION_HASH_FORCED
+#define GAME_NETVERSION "0.6 " GAME_NETVERSION_HASH
+#define LAST_COMPILE_DATE __DATE__
 static const char GAME_RELEASE_VERSION[8] = "0.6.4";
 
 #endif
